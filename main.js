705931ec0457ddf19c2b72f4c7deadf25b398c9e
Molepo21 <kgamakigift@gmail.com>
2023-11-02 16:05:18 +0200

    i added images to the web page

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
-const ourArray = [];
-let i = 0;
+function signInUser(e) {
+  e.preventDefault()
 
-do {
-  ourArray.push(i);
-  i++;
-} while (i < 5);
-console.log(ourArray);
+  // retrieving data 
+  var bookname = document.getElementById("bookname").value;
+  var author = document.getElementById("author").value;
+  var description = document.getElementById("description").value;
+  var genre = document.getElementById("genre").value;
+
+  // storing the user data in local storage
+
+  var database = []
+  database = JSON.parse(localStorage.getItem("db")) || [];
+  var user = {bookname, author,description,genre};
+  database.push(user)
+
+  // storing data
+
+  localStorage.setItem("db", JSON.stringify(database));
+  alert("Sign up successful, pease login");
+
+}
+
+function searchBook() {
+  const searchInput = document.getElementById('searchInput');
+  const searchQuery = searchInput.value.toLowerCase();
+  
+  if (searchQuery.trim() === '') {
+      alert('Please enter a search query.');
+      return;
+  }
+  
+  const names = localStorage.getItem('names') ? JSON.parse(localStorage.getItem('names')) : [];
+  const matchingNames = names.filter(name => name.toLowerCase().includes(searchQuery));
+  
+  displayResults(matchingNames);
+}
+
+function displayResults(results) {
+  const resultDiv = document.getElementById('result');
+  
+  if (results.length === 0) {
+      resultDiv.innerHTML = 'No matching names found.';
+  } else {
+      resultDiv.innerHTML = '<h2>Matching Names:</h2>' + results.join('<br>');
+  }
+}
+
+
